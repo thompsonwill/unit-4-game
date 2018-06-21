57e19373acb2e717f6c81c4a83ed83a8599bfe98
Will <williamjamesthompson@gmail.com>
2018-06-21 12:01:51 -0500

    Final game upload

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,9 @@
 $(document).ready(function() {
 
-//Initialize the game
-function initializeGame() {
-      userNum = "";
-      userScore = "";
-      var minNumber = 9;
-      var maxNumber = 120;
-      var randomNumber = randomNumberFromRange(minNumber, maxNumber);
-      $("#random-number").text(randomNumber);
-    }
-
 //Setting up variables to create our computer's random number function
-var minNumber = 9;
-var maxNumber = 120;
-var randomNumber = randomNumberFromRange(minNumber, maxNumber);
+var minNumber = 0;
+var maxNumber = 0;
+var randomNumber = 0;
 
 
 //Creates our random number for the computer's
@@ -21,19 +11,59 @@ function randomNumberFromRange(min, max) {
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-console.log(randomNumber);
+
 
 // Puts our random number in the document.
 $("#random-number").text(randomNumber);
 
 //Setting up the variables for functionality
-var userScore = 0;
-var youWin = false;
+var userScore = "";
 var userWins = 0;
 var userLoss = 0;
 
+//Setting up the crystal's random numbers
+var crysMinNum = "";
+var crysMaxNum = "";
+
+//Store the random numbers in variables
+var crysRandomNum1 = "";
+var crysRandomNum2 = "";
+var crysRandomNum3 = "";
+var crysRandomNum4 = "";
+
+//Generate our crystal's random number
+function randomNumberFromRange(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
+//Initialize the game
+function initializeGame() {
+  userNum = 0;
+  userScore = 0;
+  minNumber = 9;
+  maxNumber = 120;
+  randomNumber = randomNumberFromRange(minNumber, maxNumber);
+  $("#random-number").text(randomNumber);
+  $("#user-score").text(userScore);
+
+
+  crysMinNum = 1;
+  crysMaxNum = 12;
+  crysRandomNum1 = randomNumberFromRange(crysMinNum, crysMaxNum);
+  crysRandomNum2 = randomNumberFromRange(crysMinNum, crysMaxNum);
+  crysRandomNum3 = randomNumberFromRange(crysMinNum, crysMaxNum);
+  crysRandomNum4 = randomNumberFromRange(crysMinNum, crysMaxNum);
+  console.log("Crystal 1 - " + crysRandomNum1);
+  console.log("Crystal 2 - " + crysRandomNum2);
+  console.log("Crystal 3 - " + crysRandomNum3);
+  console.log("Crystal 4 - " + crysRandomNum4);
+}
 //Displying the crystals number on the document. Oh boy this is ugly.
 //Find out how to make this into a function. It's crazy convoluted right now. 
+
+initializeGame();
+
 
 $("#crystal1").on("click", function() {
   //Checks userScore and adds the current value of the crystal
@@ -41,20 +71,39 @@ $("#crystal1").on("click", function() {
   $("#user-score").text(userScore);
   //Checking if you win and adds your win to the scoreboard
   if (userScore === randomNumber) {
-  $("#gameStatus").text("You win!");
-  youWin = true;
-  if (youWin === true){
+    $("#gameStatus").text("You Win!");
     userWins ++;
   $("#userWins").text(userWins);
-}
+  initializeGame();
 }
   //Checks if you have lost
   if (userScore > randomNumber) {
-    $("#gameStatus").text("You lose!");
+    $("#gameStatus").text("You Lose!");
     initializeGame();
+    userLoss++;
+    $("#userLoss").text(userLoss);
   }
 });
 
+$("#crystal1").on("click", function() {
+  //Checks userScore and adds the current value of the crystal
+  userScore = userScore + crysRandomNum1;
+  $("#user-score").text(userScore);
+  //Checking if you win and adds your win to the scoreboard
+  if (userScore === randomNumber) {
+    $("#gameStatus").text("You Win!");
+    userWins ++;
+  $("#userWins").text(userWins);
+  initializeGame();
+}
+  //Checks if you have lost
+  if (userScore > randomNumber) {
+    $("#gameStatus").text("You Lose!");
+    initializeGame();
+    userLoss++;
+    $("#userLoss").text(userLoss);
+  }
+});
 
 $("#crystal2").on("click", function() {
   //Checks userScore and adds the current value of the crystal
@@ -62,20 +111,19 @@ $("#crystal2").on("click", function() {
   $("#user-score").text(userScore);
   //Checking if you win and adds your win to the scoreboard
   if (userScore === randomNumber) {
-  $("#gameStatus").text("You win!");
-  youWin = true;
-  if (youWin === true){
+    $("#gameStatus").text("You Win!");
     userWins ++;
   $("#userWins").text(userWins);
-}
+  initializeGame();
 }
   //Checks if you have lost
   if (userScore > randomNumber) {
-    $("#gameStatus").text("You lose!");
+    $("#gameStatus").text("You Lose!");
     initializeGame();
+    userLoss++;
+    $("#userLoss").text(userLoss);
   }
 });
-
 
 $("#crystal3").on("click", function() {
   //Checks userScore and adds the current value of the crystal
@@ -83,20 +131,19 @@ $("#crystal3").on("click", function() {
   $("#user-score").text(userScore);
   //Checking if you win and adds your win to the scoreboard
   if (userScore === randomNumber) {
-  $("#gameStatus").text("You win!");
-  youWin = true;
-  if (youWin === true){
+    $("#gameStatus").text("You Win!");
     userWins ++;
   $("#userWins").text(userWins);
-}
+  initializeGame();
 }
   //Checks if you have lost
   if (userScore > randomNumber) {
-    $("#gameStatus").text("You lose!");
+    $("#gameStatus").text("You Lose!");
     initializeGame();
+    userLoss++;
+    $("#userLoss").text(userLoss);
   }
 });
-
 
 $("#crystal4").on("click", function() {
   //Checks userScore and adds the current value of the crystal
@@ -104,40 +151,18 @@ $("#crystal4").on("click", function() {
   $("#user-score").text(userScore);
   //Checking if you win and adds your win to the scoreboard
   if (userScore === randomNumber) {
-  $("#gameStatus").text("You win!");
-  youWin = true;
-  if (youWin === true){
+    $("#gameStatus").text("You Win!");
     userWins ++;
   $("#userWins").text(userWins);
+  initializeGame();
 }
-}
-  //Checks if you have lost and adds a loss to userLoss variable
+  //Checks if you have lost
   if (userScore > randomNumber) {
-    userLoss ++;
-    $("#gameStatus").text("You lose!");
+    $("#gameStatus").text("You Lose!");
+    initializeGame();
+    userLoss++;
     $("#userLoss").text(userLoss);
   }
 });
 
-
-//Setting up the crystal's random numbers
-var crysMinNum = 1;
-var crysMaxNum = 12;
-
-//Store the random numbers in variables
-var crysRandomNum1 = randomNumberFromRange(crysMinNum, crysMaxNum);
-var crysRandomNum2 = randomNumberFromRange(crysMinNum, crysMaxNum);
-var crysRandomNum3 = randomNumberFromRange(crysMinNum, crysMaxNum);
-var crysRandomNum4 = randomNumberFromRange(crysMinNum, crysMaxNum);
-
-//Generate our crystal's random number
-function randomNumberFromRange(min, max) {
-  return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-//Store the random numbers in variables
-console.log(crysRandomNum1);
-console.log(crysRandomNum2);
-console.log(crysRandomNum3);
-console.log(crysRandomNum4);
 });
